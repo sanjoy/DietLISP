864cd681d5266e7915ff6228985fe2b501bc3817
Sanjoy Das <sanjoy@playingwithpointers.com>
2011-12-13 04:10:40 +0530

    Add a python script to run tests, in llvm-lit style.

---
+++ samples/test.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+# Testing framework for DietLISP, based on llvm-lit.
+
+import os
+import string
+import subprocess
+import sys
+
+def parse(file_obj):
+    command = None
+    outputs = []
+    command_prefix = ';; RUN:'
+    output_prefix = ';; EXPECT:'
+
+    for line in file_obj:
+        s_line = line.strip()
+        if s_line.startswith(command_prefix):
+            if command is None:
+                command = s_line[len(command_prefix):].lstrip()
+            else:
+                print 'Multiple `RUN` strings.'
+                return None
+        elif s_line.startswith(output_prefix):
+            outputs.append(s_line[len(output_prefix):].lstrip())
+
+    if command is None:
+        print 'Could not find `RUN` string.'
+        return None
+    return (command, string.join(outputs, '\n'))
+
+def test(command, output):
+    try:
+        pipe = subprocess.Popen([command], shell=True, stdout=subprocess.PIPE)
+        pipe.wait()
+        to_test = pipe.stdout.read().strip()
+        if to_test == output:
+            return True
+        print 'DietLISP FAIL!'
+        print '**************\n'
+        print 'Expected output: \n'
+        print output
+        print '\nOutput received: \n'
+        print to_test
+        print ''
+    except OSError:
+        print 'Failed to execute command `' + command + '`'
+        return False
+
+def process_file(file_name):
+    print 'Testing `' + file_name + '`'
+    try:
+        f = open(file_name, 'r')
+    except IOError:
+        print 'Could not open file: `' + file_name + '`'
+        return
+    
+    p = parse(f)
+    f.close()
+
+    if p is None:
+        print 'Error parsing file `' + file_name + '`'
+        return
+
+    (cmd, output) = p
+    cmd = cmd.replace('<file>', file_name)
+
+    result = test(cmd, output)
+    if not result:
+        print 'Failed test `' + file_name + '`'
+
+def usage():
+    print 'Usage: ' + sys.argv[0] + ' [--all] [file_name]'
+
+def main():
+    if len(sys.argv) < 2:
+        usage()
+        return
+    if sys.argv[1] == '--all':
+        for filename in os.listdir(os.getcwd()):
+            if filename.endswith('.dlisp'):
+                process_file(filename)
+    else:
+        process_file(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
