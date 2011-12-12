1fe449029a729b7ea0a5556fd1ae3809906a7dea
Sanjoy Das <sanjoy@playingwithpointers.com>
2011-12-13 04:39:59 +0530

    Change extension of tests / examples to .dlisp. Move a few test cases into Basics0.dlisp.

--- samples/Primality.lisp
+++
@@ -1,9 +0,0 @@
-;; Checks if `number` is prime
-(let ((number 7)
-      (check (Y (lambda (self n current)
-                  (if (>= current (/ n 2))
-                      true
-                      (if (zerop (% n current))
-                          false
-                          (self n (+ 1 current))))))))
-  (check number 2))
